74c296575c57cd1cb221f294a6e98af3916790ac
BreinerJT <breinerjtorres@gmail.com>
2022-06-27 21:01:28 -0500

    Final

---
+++ app.js
@@ -0,0 +1,27 @@
+const form = document.querySelector(".form")
+const email = document.querySelector("#email")
+const warn = document.querySelector(".warn")
+const warnMsg = document.querySelector("#label")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    validateForm()
+})
+
+function validateForm() {
+    const value = email.value
+    let rgx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i
+    const status = value.match(rgx)
+    if(!status || value == ""){
+        warn.classList.remove("hide")
+        warnMsg.classList.remove("hide")
+    }
+    else{
+        warn.classList.add("hide")
+        warnMsg.classList.add("hide")
+    }
+    setTimeout(() => {
+        warn.classList.add("hide")
+        warnMsg.classList.add("hide")
+    }, 2000)
+}
